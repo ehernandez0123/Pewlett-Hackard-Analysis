96b93f8e65bcfe4a0108955e707e906bd7a20611
Estefany Hernandez <estefanyhernandez@Estefanys-Air.lan>
2023-01-22 21:29:31 -0600

    Adding updated files to folder

--- schema.sql
+++ schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE employees(
 
 -- Creating a managers table
 CREATE TABLE dept_manager (
-	dept_no VARCHAR(4) NOT NULL,
+dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE salaries (
 );
 
 -- Creating a titles table
-CREATE TABLE titles(
+CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
@@ -50,14 +50,17 @@ CREATE TABLE titles(
 );
 
 -- Creating a Dept Emp
-CREATE TABLE dept_emp(
-	emp_no INT NOT NULL,
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
-	from_date DATE NOT NULL,
-  	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
+
 
 -- Confirm the tables were created successfully by running a SELECT statement
 SELECT * FROM departments;
+
